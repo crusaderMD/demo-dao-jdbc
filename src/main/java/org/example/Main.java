af9111049556716c12598f5e5bf8c227af27738d
LBortolotti <Luiz.Bortolotti-ext@ab-Inbev.com>
2025-10-28 14:08:30 -0300

    Creating project and git repository

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import org.example.db.DB;
+import org.example.db.DbException;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
