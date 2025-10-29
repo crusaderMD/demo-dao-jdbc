d033a56a0cc7d0f1c7619b5baab2c829052a18b6
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2025-10-29 09:11:04 -0300

    Department entity class

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 import org.example.db.DB;
 import org.example.db.DbException;
+import org.example.model.entities.Department;
 
 import java.sql.Connection;
 import java.sql.SQLException;
@@ -10,5 +11,8 @@ import java.sql.Statement;
 public class Main {
     public static void main(String[] args) {
 
+        Department obj = new Department(1, "Books");
+
+        System.out.println(obj);
     }
 }
