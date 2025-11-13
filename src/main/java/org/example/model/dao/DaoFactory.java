704161e3e873fcbdd205007772885b4da9e358e8
Luiz R Bortolotti Junior <luiz_bortolotti@hotmail.com>
2025-11-13 12:21:49 -0300

    DaoFactory implementation

---
+++ src/main/java/org/example/model/dao/DaoFactory.java
@@ -0,0 +1,18 @@
+package org.example.model.dao;
+
+import org.example.db.DB;
+import org.example.model.dao.impl.DepartmentDaoJDBC;
+import org.example.model.dao.impl.SellerDaoJDBC;
+
+import java.sql.Connection;
+
+public class DaoFactory {
+
+    public static SellerDao createSellerDao(){
+        return new SellerDaoJDBC(DB.getConnection());
+    }
+
+    public static  DepartmentDao createDepartmentDao() {
+        return new DepartmentDaoJDBC(DB.getConnection());
+    }
+}
